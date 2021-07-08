b32f062e50af4f124b5a10fc32f49a460703829c
sudipachandra <85782936+sudipachandra@users.noreply.github.com>
2021-07-08 20:36:05 +0530

    Migrate to macOS (#1)

---
+++ src/main/java/com/selenium/assessment/SignIn.java
@@ -0,0 +1,60 @@
+package com.selenium.assessment;
+
+import java.io.IOException;
+import java.util.Hashtable;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.selenium.utils.ExcelReader;
+
+public class SignIn {
+	static WebDriver driver = null;
+
+	@BeforeMethod
+	public void LaunchBrowser() {
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://automationpractice.com/index.php");
+	}
+
+	@Test(dataProvider = "getLogindetails")
+	public void enterSignIn(Hashtable<String, String> data) throws InterruptedException {
+		WebElement signInLink = driver.findElement(By.className("login"));
+		signInLink.click();
+		Thread.sleep(3000);
+		WebElement emailBtn = driver.findElement(By.id("email"));
+		emailBtn.sendKeys(data.get("username"));
+		WebElement passWordBtn = driver.findElement(By.id("passwd"));
+		passWordBtn.sendKeys(data.get("passward"));
+		driver.findElement(By.id("SubmitLogin")).click();
+
+	}
+
+	@DataProvider
+	public Object[][] getLogindetails() throws IOException {
+		String ProjectPath = System.getProperty("user.dir");// this will return project current directory path
+		System.out.println("ProjectPath = " + ProjectPath);
+		String filepath = ProjectPath + "/src/main/java/com/selenium/testdata";
+		String filename = "LoginData.xlsx";
+		String sheetname = "LoginDetails";
+		return ExcelReader.ReadExcelDataToObjArray(filepath, filename, sheetname);
+	}
+
+	@AfterMethod
+	public void closeBrowser() throws InterruptedException {
+		Thread.sleep(3000);
+		if (driver != null) {
+			driver.close();
+			driver.quit();
+		}
+	}
+
+}
---
+++ src/main/java/com/selenium/testcases/ProductSearchTestCase.java
@@ -0,0 +1,62 @@
+package com.selenium.testcases;
+
+import java.io.IOException;
+import java.util.Hashtable;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.selenium.pages.SearchBox;
+import com.selenium.pages.SignInPage;
+import com.selenium.utils.ExcelReader;
+
+public class ProductSearchTestCase {
+	private WebDriver driver;
+	private SearchBox searchBox;
+
+	@BeforeMethod
+	public void launchBrowser() {
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver");
+		driver = new ChromeDriver();
+		searchBox = new SearchBox(driver);
+		driver.get("http://automationpractice.com/index.php");
+		// maximize my window
+		driver.manage().window().maximize();
+	}
+
+	@Test(dataProvider = "getSearchdetails")
+	public void searchProduct(Hashtable<String, String> data) throws InterruptedException {
+		SignInPage signInPage = new SignInPage(driver);
+		Thread.sleep(3000);
+		searchBox.search(data.get("searchvalue"));
+		String actualResult = signInPage.resultBox.getText();
+		System.out.println("the expected result is: " + actualResult);
+		String expectResult = data.get("expectedno");
+		System.out.println("the actual result is: " + expectResult);
+		Assert.assertEquals(actualResult, expectResult);
+	}
+
+	@DataProvider
+	public Object[][] getSearchdetails() throws IOException {
+		String ProjectPath = System.getProperty("user.dir");// this will return project current directory path
+		System.out.println("ProjectPath = " + ProjectPath);
+		String filepath = ProjectPath + "/src/main/java/com/selenium/testdata";
+		String filename = "LoginData.xlsx";
+		String sheetname = "SearchValue";
+		return ExcelReader.ReadExcelDataToObjArray(filepath, filename, sheetname);
+	}
+
+	@AfterMethod
+	public void closeBrowser() throws InterruptedException {
+		Thread.sleep(3000);
+		if (driver != null) {
+			driver.close();
+			driver.quit();
+		}
+	}
+}
